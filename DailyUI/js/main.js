9c7de1f7f39e6a7602bc61d08d6c900958419c60
Andriy Polukhin <andriy.polukhin@gmail.com>
2019-03-01 19:18:49 +0200

    test projects

--- DailyUI/js/main.js
+++ DailyUI/js/main.js
@@ -1,42 +1,37 @@
-$('.input-cart-number').on('keyup change', function () {
-  $t = $(this);
+val = 10 + Math.random() * 20;
 
-  if ($t.val().length > 3) {
-    $t.next().focus();
+function createStaff() {
+  $('<div class="rs-gradient" />').insertBefore($('.rs-tooltip'));
+}
+
+
+function update() {
+  val = -230 + parseInt($('.rs-tooltip-text').html() * 7.5);
+  $('.rs-gradient').css({
+    background: 'hsl(' + val + ', 100%, 57%)'
+  });
+}
+
+
+
+$("#slider").roundSlider({
+  radius: 80,
+  width: 15,
+  min: 10,
+  max: 30,
+  circleShape: "pie",
+  handleSize: "+16",
+  handleShape: "dot",
+  sliderType: "min-range",
+  startAngle: 315,
+  value: val,
+  editableTooltip: false,
+  mouseScrollAction: true,
+  change: update,
+  drag: update,
+  create: function () {
+    createStaff();
+    update();
   }
 
-  var card_number = '';
-  $('.input-cart-number').each(function () {
-    card_number += $(this).val() + ' ';
-    if ($(this).val().length == 4) {
-      $(this).next().focus();
-    }
-  })
-
-  $('.credit-card-box .number').html(card_number);
-});
-
-$('#card-holder').on('keyup change', function () {
-  $t = $(this);
-  $('.credit-card-box .card-holder div').html($t.val());
-});
-
-$('#card-holder').on('keyup change', function () {
-  $t = $(this);
-  $('.credit-card-box .card-holder div').html($t.val());
-});
-
-$('#card-expiration-month, #card-expiration-year').change(function () {
-  m = $('#card-expiration-month option').index($('#card-expiration-month option:selected'));
-  m = (m < 10) ? '0' + m : m;
-  y = $('#card-expiration-year').val().substr(2, 2);
-  $('.card-expiration-date div').html(m + '/' + y);
 })
-
-$('#card-ccv').on('focus', function () {
-  $('.credit-card-box').addClass('hover');
-}).on('blur', function () {
-  $('.credit-card-box').removeClass('hover');
-}).on('keyup change', function () {
-  $('.ccv div').html($(this).val());
-});
