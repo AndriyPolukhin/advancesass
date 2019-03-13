42d30dea5be7aea10bec35ff13b60ed1ee134ab0
Andriy Polukhin <andriy.polukhin@gmail.com>
2019-03-13 22:38:14 +0200

    infinite unsplash

---
+++ DailyReactUI/unsplashscroll/config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || 'UNSPLASH_API_KEY',
+  SECRET: process.env.SECRET || 'UNSPLASH_API_SECRET',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+
+};
